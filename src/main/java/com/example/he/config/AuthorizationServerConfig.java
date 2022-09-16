d1799da340376bd5e2119d113db51728426831a4
hezhongfeng <287645587@qq.com>
2022-09-16 10:39:58 +0800

    🐞 fix: fix value

--- src/main/java/com/example/he/config/AuthorizationServerConfig.java
+++ src/main/java/com/example/he/config/AuthorizationServerConfig.java
@@ -13,7 +13,6 @@
  */
 package com.example.he.config;
 
-import java.util.UUID;
 import com.example.he.jose.Jwks;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
@@ -24,23 +23,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.core.oidc.OidcScopes;
-import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationConsentService;
-import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
-import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
-import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
-import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.config.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
@@ -66,44 +50,6 @@ public class AuthorizationServerConfig {
 		return http.build();
 	}
 
-	// @formatter:off
-	// @Bean
-	// public RegisteredClientRepository registeredClientRepository(JdbcTemplate jdbcTemplate) {
-	// 	RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
-	// 			.clientId("messaging-client")
-	// 			.clientSecret("{noop}secret")
-	// 			.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-	// 			.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-	// 			.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-	// 			.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-	// 			.redirectUri("http://127.0.0.1:8080/login/oauth2/code/messaging-client-oidc")
-	// 			.redirectUri("http://127.0.0.1:8080/authorized")
-	// 			.scope(OidcScopes.OPENID)
-	// 			.scope("message.read")
-	// 			.scope("message.write")
-	// 			.clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
-	// 			.build();
-
-	// 	// Save registered client in db as if in-memory
-	// 	JdbcRegisteredClientRepository registeredClientRepository = new JdbcRegisteredClientRepository(jdbcTemplate);
-	// 	registeredClientRepository.save(registeredClient);
-
-	// 	return registeredClientRepository;
-	// }
-	// @formatter:on
-
-	// @Bean
-	// public OAuth2AuthorizationService authorizationService(JdbcTemplate jdbcTemplate,
-	// RegisteredClientRepository registeredClientRepository) {
-	// return new JdbcOAuth2AuthorizationService(jdbcTemplate, registeredClientRepository);
-	// }
-
-	// @Bean
-	// public OAuth2AuthorizationConsentService authorizationConsentService(JdbcTemplate jdbcTemplate,
-	// RegisteredClientRepository registeredClientRepository) {
-	// return new JdbcOAuth2AuthorizationConsentService(jdbcTemplate, registeredClientRepository);
-	// }
-
 	@Bean
 	public JWKSource<SecurityContext> jwkSource() {
 		RSAKey rsaKey = Jwks.generateRsa();
@@ -115,16 +61,4 @@ public class AuthorizationServerConfig {
 	public ProviderSettings providerSettings() {
 		return ProviderSettings.builder().issuer("http://localhost:9000").build();
 	}
-
-	// @Bean
-	// public EmbeddedDatabase embeddedDatabase() {
-	// 	// @formatter:off
-	// 	return new EmbeddedDatabaseBuilder()
-	// 			.generateUniqueName(true)
-	// 			.setScriptEncoding("UTF-8")
-	// 			.addScript("src/main/resources/auth-he.sql")
-	// 			.build();
-	// 	// @formatter:on
-	// }
-
 }
