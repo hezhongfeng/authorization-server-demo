dc5b5626d0b6eeef5dc2964319434a5c0780c43f
hezhongfeng <287645587@qq.com>
2022-09-28 14:53:50 +0800

    🐞 fix: fix client id

--- src/main/java/com/example/he/util/RegisteredClients.java
+++ src/main/java/com/example/he/util/RegisteredClients.java
@@ -1,17 +1,15 @@
 /*
  * Copyright 2020-2022 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *      https://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.example.he.util;
 
@@ -19,11 +17,9 @@ import java.util.UUID;
 
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.core.OAuth2TokenFormat;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.config.ClientSettings;
-import org.springframework.security.oauth2.server.authorization.config.TokenSettings;
 
 /**
  * @author Steve Riesenberg
@@ -43,7 +39,6 @@ public class RegisteredClients {
 				.scope("message.read")
 				.scope("message.write")
 				.clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
-				.tokenSettings(TokenSettings.builder().accessTokenFormat(OAuth2TokenFormat.SELF_CONTAINED).build())
 				.build();
 	}
 	// @formatter:on
--- src/test/java/com/example/he/HeApplicationTests.java
+++ src/test/java/com/example/he/HeApplicationTests.java
@@ -23,14 +23,14 @@ class HeApplicationTests {
 		client.setId(UUID.randomUUID().toString());
 		// client.setClientIdIssuedAt(Instant.now());
 		client.setClientName("name");
-		client.setClientId("message-client");
+		client.setClientId("messaging-client");
 		client.setClientSecret("{noop}secret");
 		client.setRedirectUris(
 				"http://127.0.0.1:8080/login/oauth2/code/messaging-client-oidc,http://127.0.0.1:8080/authorized");
 		client
 				.setClientAuthenticationMethods(ClientAuthenticationMethod.CLIENT_SECRET_BASIC.getValue());
-		client.setAuthorizationGrantTypes(AuthorizationGrantType.AUTHORIZATION_CODE.getValue() + ","
-				+ AuthorizationGrantType.REFRESH_TOKEN.getValue() + ","
+		client.setAuthorizationGrantTypes(AuthorizationGrantType.REFRESH_TOKEN.getValue() + ","
+				+ AuthorizationGrantType.AUTHORIZATION_CODE.getValue() + ","
 				+ AuthorizationGrantType.CLIENT_CREDENTIALS.getValue());
 		client.setScopes("openid,message.read,message.write");
 
@@ -38,7 +38,7 @@ class HeApplicationTests {
 				"{\"@class\":\"java.util.Collections$UnmodifiableMap\",\"settings.client.require-proof-key\":false,\"settings.client.require-authorization-consent\":true}";
 		String tokenSettingString =
 				"{\"@class\":\"java.util.Collections$UnmodifiableMap\",\"settings.token.reuse-refresh-tokens\":true,\"settings.token.id-token-signature-algorithm\":[\"org.springframework.security.oauth2.jose.jws.SignatureAlgorithm\",\"RS256\"],\"settings.token.access-token-time-to-live\":[\"java.time.Duration\",300.000000000],\"settings.token.access-token-format\":{\"@class\":\"org.springframework.security.oauth2.core.OAuth2TokenFormat\",\"value\":\"self-contained\"},\"settings.token.refresh-token-time-to-live\":[\"java.time.Duration\",3600.000000000]}";
-		//  "{\"@class\":\"java.util.Collections$UnmodifiableMap\",\"settings.token.reuse-refresh-tokens\":true,\"settings.token.id-token-signature-algorithm\":[\"org.springframework.security.oauth2.jose.jws.SignatureAlgorithm\",\"RS256\"],\"settings.token.access-token-time-to-live\":[\"java.time.Duration\",300.000000000],\"settings.token.refresh-token-time-to-live\":[\"java.time.Duration\",3600.000000000]}";
+
 
 		client.setClientSettings(clientSettingString);
 		client.setTokenSettings(tokenSettingString);
