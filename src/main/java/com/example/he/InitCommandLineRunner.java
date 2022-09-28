2945eef46d3af7d71e5cdd230abdd16fffc8fc16
hezhongfeng <287645587@qq.com>
2022-09-28 11:17:44 +0800

    ✨ feat: 初始化sql文件

--- src/main/java/com/example/he/InitCommandLineRunner.java
+++ src/main/java/com/example/he/InitCommandLineRunner.java
@@ -2,10 +2,13 @@ package com.example.he;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import com.example.he.oauth.entity.Client;
+import com.example.he.oauth.repository.ClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import static com.example.he.util.RegisteredClients.messagingClient;
+import java.util.List;
 
 @Component
 public class InitCommandLineRunner implements CommandLineRunner {
@@ -13,11 +16,21 @@ public class InitCommandLineRunner implements CommandLineRunner {
   @Autowired
   private RegisteredClientRepository registeredClientRepository;
 
+  @Autowired
+  private ClientRepository clientRepo;
+
   @Override
   public void run(String... args) throws Exception {
     System.out.println("HHHHH555ApplicationRunner");
 
-    RegisteredClient registeredClient = messagingClient();
-    this.registeredClientRepository.save(registeredClient);
+
+    List<Client> clients = clientRepo.findAll();
+
+    for (Client client : clients) {
+      System.out.print(client.getClientName());
+    }
+
+    // RegisteredClient registeredClient = messagingClient();
+    // this.registeredClientRepository.save(registeredClient);
   }
 }
